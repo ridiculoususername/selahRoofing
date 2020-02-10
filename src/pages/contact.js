159d89b6c77535599faa2b31b4006558130607ea
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-10 00:34:28 -0800

    Added contact page view.

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => {
+    return (
+        <Layout>
+        <SEO title="Contact" />
+        <div>
+            <h1>Contact</h1>
+            <h2>Get in touch!</h2>
+            <p>If you live in Los Angeles, CA and are looking for a roofing contractor, look no further than Selah Roofing. Call us any time.</p>
+        </div>
+        </Layout>
+    )
+}
+
+export default ContactPage;
