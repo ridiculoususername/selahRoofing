9141d8f1771b948ab4cdab08eccb997ebab1dded
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-09 18:33:15 -0800

    Changed site metadata in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Selah Roofing`,
+    description: `Selah Roofing is a roofing service based out of commerce california. Call now to see if we can meet your roofing needs`,
+    author: `@lorenzolevy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
