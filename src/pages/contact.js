155cfe13e8f0f5c370d87fea17622582ef8d8d79
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-10 00:34:28 -0800

    Added Menu component, cleaned up view syntax.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = () => {
-    return (
+const ContactPage = () => (
         <Layout>
         <SEO title="Contact" />
         <div>
@@ -12,7 +11,7 @@ const ContactPage = () => {
             <p>If you live in Los Angeles, CA and are looking for a roofing contractor, look no further than Selah Roofing. Call us any time.</p>
         </div>
         </Layout>
-    )
-}
+)
+
 
 export default ContactPage;
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ServicesPage = () => {
-    return (
+const ServicesPage = () => (
         <Layout>
         <SEO title="Services" />
         <div>
@@ -12,7 +11,6 @@ const ServicesPage = () => {
             <p>When it comes to residential roofing installation, inspection, and repairs, making sure you get the best service possible is important. You want low-price roof repair and service without sacrificing quality. If you are looking for residential roofing in Los Angeles, CA, look no further than Selah Roofing. Our roofers are professional. They can assist with leak repair and roof remodeling and work with a variety of roofing materials. Installing a roof during new construction or a remodel takes some thought on the part of our clients: what kind of roof do you want? There are many materials available including tile roofing, shingle roofing, slate, and clay. Some of the most popular options include asphalt shingles, wood shakes and shingles, and metal. All have great benefits and all are reasonably priced. No material is more capable of withstanding wind, hail, fire, rain, or rot than metal roofing. Metal roofing is energy-efficient, allowing you to cut back on your monthly energy use, and it can be fashioned into a vast selection of styles and designs. Asphalt shingles are economical and easily installed. They are affordable because of their wide availability and offer your home protection from harmful UV rays. Like metal, they come in a full range of styles and colors to complement your home. At Selah Roofing, we know the importance of a good roof. We work efficiently to make sure that your product is installed on time and correctly, double- and triple-checking everything to make sure that the roof is structurally sound. We will also make sure that your project stays on or below budget. If this sounds like a good deal to you, then reach us any time for residential roofing in the Los Angeles, CA area.</p>
         </div>
         </Layout>
-    )
-}
+)
 
 export default ServicesPage;
