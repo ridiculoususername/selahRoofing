149b1ba5e2483e65ed3086447eca5b2ae22619f8
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-17 02:12:36 -0800

    adding link animations and color interactions

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,10 +5,10 @@ import menuStyles from '../styles/menu.module.scss'
 const Menu = () => (
         <div>
         <ul className={menuStyles.navList}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/news">Projects</Link></li>
+            <li><Link className={menuStyles.navItem} activeClassName={menuStyles.activeNavItem} to="/">Home</Link></li>
+            <li><Link className={menuStyles.navItem} activeClassName={menuStyles.activeNavItem} to="/contact">Contact</Link></li>
+            <li><Link className={menuStyles.navItem} activeClassName={menuStyles.activeNavItem} to="/services">Services</Link></li>
+            <li><Link className={menuStyles.navItem} activeClassName={menuStyles.activeNavItem} to="/news">Projects</Link></li>
         </ul>
         </div>
 )
