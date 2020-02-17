e176a1497443378976eeff1593471760240513fb
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-17 01:45:33 -0800

    Re-organized to use modular scss.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,48 +6,22 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import Menu from "./menu"
-import "./layout.scss"
+import "../styles/global.scss"
+import layoutStyles from '../styles/layout.module.scss'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <Menu />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+    <div className={layoutStyles.container}>
+      <div>
+        <Header />
+        <Menu />
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
-    </>
+    </div>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
