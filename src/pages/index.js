e6b0bc843aca33d1fc237f0aa9b5d4efa4e73267
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-02-17 22:24:07 -0800

    Changed README and INDEX from starter defaults

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,8 +9,8 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Welcome</h1>
     <p></p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+     
     </div>
   </Layout>
 )
